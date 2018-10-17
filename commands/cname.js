05dc633f65009448b0d645185ab17bf81c58e106
Personne17 <44241269+Personne17@users.noreply.github.com>
2018-10-17 20:43:09 +0200

    Update cname.js

--- commands/cname.js
+++ commands/cname.js
@@ -2,10 +2,17 @@ const Discord = require('discord.js');
 
 
 
-if(!msg.guild.member(msg.author).hasPermission("ADMINISTRATOR")) {
-            msg.channel.send("Vous n'avez pas les permissions requises pour effectuer cette commande. ('ADMIN')")
-        }
-        msg.guild.channels.map(c => {
-            msg.channel.send(c + ",\n")
-        })
-    }
+
+module.exports.run = (bot, message, args) => {
+     if(!msg.guild.member(msg.author).hasPermission("ADMINISTRATOR")) {
+                        msg.channel.send("Vous n'avez pas les permissions requises pour effectuer cette commande. ('ADMIN')")
+            }
+            msg.guild.channels.map(c => {
+                        msg.channel.send(c + ",\n")
+            })
+      }    
+}
+
+module.exports.help = {
+  name: "cname"
+}

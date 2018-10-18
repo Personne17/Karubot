58968b47a80ece5913aeebe725eaf7be830ba724
Personne17 <44241269+Personne17@users.noreply.github.com>
2018-10-18 22:28:54 +0200

    Add files via upload

---
+++ commands/ban.js
@@ -0,0 +1,37 @@
+
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args) => {
+
+    message.delete();
+if(!message.member.hasPermission("BAN_MEMBERS")) return errors.noPerms(message, "BAN_MEMBERS");
+if(args[0] == "help"){
+  message.reply("Usage: !ban <user> <reason>");
+  return;
+}
+let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+if(!bUser) return errors.cantfindUser(message.channel);
+if(bUser.id === bot.user.id) return errors.botuser(message); 
+let bReason = args.join(" ").slice(22);
+if(!bReason) return errors.noReason(message.channel);
+if(bUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, bUser, "MANAGE_MESSAGES");
+
+let banEmbed = new Discord.RichEmbed()
+.setDescription("Ban !")
+.setColor("#bc0000")
+.addField("Utilisateur Banni :", `${bUser} with ID ${bUser.id}`)
+.addField("Banni par :", `<@${message.author.id}> with ID ${message.author.id}`)
+.addField("Banni dans :", "message.channel")
+.addField("A :", message.createdAt)
+.addField("Raison :", bReason);
+
+let incidentchannel = message.guild.channels.find(`name`, "incidents");
+if(!incidentchannel) return message.channel.send("veuillez créér un salon nommé 'incidents' pour pouvoir effectuer cette commande.");
+
+message.guild.member(bUser).ban(bReason);
+incidentchannel.send(banEmbed);
+
+}
+module.exports.help = {
+  name: "ban"
+}

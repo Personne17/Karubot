fcb391db67944b07c8b946f1717fdeaaf373b08b
Personne17 <44241269+Personne17@users.noreply.github.com>
2018-10-18 13:09:29 +0200

    Add files via upload

--- commands/clear.js
+++ commands/clear.js
@@ -4,11 +4,11 @@ const delay = require("timeout-as-promise");
 module.exports.run = (bot, message, args) => {
     
     let msg = message;
-    if(args > 100) return msg.channel.send("Le nombre maximum de messages pouvant être supprimmés est de 100")
+    if(args >= 100) return msg.channel.send("Le nombre maximum de messages pouvant être supprimmés est de 99")
     if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n'avez pas les permissions requises pour effectuer cette commande. ('MANAGE_MESSAGES') !");
 
     if(!args[0]) return message.channel.send("Tu dois indiquer le nombre de messages à surpprimer !")
-    message.channel.bulkDelete(args[0]).then(() => {
+    message.channel.bulkDelete(args[0] + 1).then(() => {
         delay(1000).then(function() {
             message.channel.send(`${args[0]} messages ont été supprimés !`)       
         })

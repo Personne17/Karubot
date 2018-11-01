f5943c879932980818c2093bb4ca9a6f80288399
Personne17 <44241269+Personne17@users.noreply.github.com>
2018-11-01 02:36:08 +0100

    Update ibot.js

--- commands/ibot.js
+++ commands/ibot.js
@@ -9,6 +9,7 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail(boticon)
     .addField("Nom du bot :", bot.user.username)
     .addField("Créé le :", bot.user.createdAt);
+    .addField("Créé par :", "`_Personn_#0524`")
 
     message.channel.send(botembed);
 }

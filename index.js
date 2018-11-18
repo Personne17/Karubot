6fb699bf062b0972f84dcf5a2b9a7b43d3ca427e
Personne17 <44241269+Personne17@users.noreply.github.com>
2018-11-18 13:22:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 const discord = require("discord.js");
 const fs = require("fs");
-const client = new discord.Client({disableEveryone: true});
+const config = require("./config.json");
+const bot = new discord.Client({disableEveryone: true});
 const token = process.env.token;
-var ffmpeg = require('ffmpeg');
-var delay = require("timeout-as-promise");
-var up = false;
+const delay = require("timeout-as-promise");
 
-
-client.on("ready", () => {
-  console.log(`${client.user.username} est prêt !`);
-  client.user.setActivity(`$help | ${client.guilds.size} serveur(s)`, {type: "WATCHING"})
-  .then(presence => console.log(`Activité : ${presence.game ? presence.game.name : 'none'}`))
+// When bot ready
+bot.on("ready", () => {
+  console.log(`${bot.user.username} est prêt à kick !`);
+  bot.user.setActivity(`.help | ${bot.guilds.size} serveur(s)`, {type: "WATCHING"})
+  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
   .catch(console.error);
-
-  while(up === true) {
-    up = false
-  delay(17000000).then(function() {
-    console.log("Up !")
-    up = true
-    })
-  }
 });
 
 // Load commands
-client.commands = new discord.Collection();
+bot.commands = new discord.Collection();
 fs.readdir("./commands/", (err, files) => {
   if (err) console.error(err);
   let jsfiles = files.filter(f => f.split(".").pop() === "js");
@@ -34,33 +25,24 @@ fs.readdir("./commands/", (err, files) => {
   jsfiles.forEach((f, i) => {
     let props = require(`./commands/${f}`);
     console.log(`${i + 1}: ${f} chargée !`);
-    client.commands.set(props.help.name, props);
+    bot.commands.set(props.help.name, props);
   });
 });
 
-client.on("guildMemberAdd", m => {
-    let bbchannel = client.channels.get("513343745735917616")
-    bbchannel.send("Hey ! " + m + " Bienvenue sur : :white_large_square::arrow_right:SERVEUR COOL:arrow_left::white_large_square: !")
-})
+// Message event
+bot.on("message", message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
 
-client.on("guildMemberRemove", mr => {
-  let rchannel = client.channels.get("513343745735917616")
-  rchannel.send(mr + ", puisse son nom rester à jamais oublié, viens de quitter le serveur. :skull:")
-})
-
-
-client.on("message", msg => {
-  if (msg.author.bot) return;
-  if (msg.channel.type === "dm") return;
-  let prefix = "$";
-  let messageArray = msg.content.split(" ");
+  let prefix = config.prefix;
+  let messageArray = message.content.split(" ");
   let command = messageArray[0].toLowerCase();
-  let guild = client.guilds.get("513343745735917614");
+  let args = messageArray.slice(1);
+
   if (!command.startsWith(prefix)) return;
 
-  let args = messageArray.slice(1);
-  let cmd = client.commands.get(command.slice(prefix.length));
-  if (cmd) cmd.run(client, msg, args, guild);
+  let cmd = bot.commands.get(command.slice(prefix.length));
+  if (cmd) cmd.run(bot, message, args);
 });
 
-client.login("NTEzNDE1MjE5OTIyMzM3Nzk4.DtHqyg.DNBfu9Bqb1Z27qhUos9S2LcLIjo");
+bot.login(token);

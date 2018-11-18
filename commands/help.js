09376be07a901aa9263480d41075b85b8d71d1e2
Personne17 <44241269+Personne17@users.noreply.github.com>
2018-11-18 13:19:53 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,29 @@
 const Discord = require("discord.js");
 
-module.exports.run = (client, msg, args, guild) => {
-  var bchannel = client.channels.get("513387249153474561");
-  var help_embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setTitle("Liste Des Commandes")
-  .setDescription("Commandes du bot :")
-  .addField(`$help`, `Vous montre ce message`, false)
-  .addField("$verif", "Vous donne le rôle : <@&513346079287410689>")
-  .addField("$ban", "Utilisation : $ban <utilisateur> <raison>.")
-bchannel.send(help_embed);
+module.exports.run = (bot, message, args) => {
+    let msg = message
+
+    var help_embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setTitle("Liste Des Commandes de Karubot")
+    .setDescription("Commandes :")
+    .addField(`.help`, ` Vous montre ce message`, true)
+    .addField(".salut", "Le bot vous réponds ! N'est-ce pas magique ?", true)
+    .addField(".invite", "Le bot vous envoie son lien d'invitation discord !", true)
+    .addField(".report + [MENTION] + [RAISON]", " Vous permet de report un autre utilisateur", false)
+    .addField(".search + [RECHERCHE]", " Vous donne un lien google vers ce que vous avez cherché.", false)
+    .addField(".si", " Vous montre toutes les infos du serveur", false)
+    .addField(".boti", "Vous montre les informations du bot", true)
+    .addField(".stats", " Vous donne des informations sur votre compte", true)
+    .addField(".ban + [Utilisateur]", "Bannis l'utilisateur spécifié.", false)
+    .addField(".tempmute + [Utilisateur] + [1s/m/h/d] + [Raison]", "Vous permet de mute un utilisateur ! (Enlevez-lui tous ses rôles) Manage_Messages requis !")
+    .addField(`.clear + [NOMBRE]`, `Supprimme le nombre de messages indiqué. ("MANAGE_MESSAGES" requis)`, false)
+    .addField(`.cdel`, `Supprimme tous les salons du serveur (Fondateur requis)`, true)
+    .addField(".cname", " Donne la liste des salons sur le serveur (Admin requis à cause de : SPAM)", true)
+    .addField(".panic", "Attention: commande très puissante. Kick tous les membres (Fondateur requis)", false)
+    .setFooter("Bot créé par _Personn_#0524.")
+
+    message.channel.send(help_embed);
 }
 module.exports.help = {
   name: "help"

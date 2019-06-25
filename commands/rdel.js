dae15cc201d064fa6ccd1570ba643b0b1cae7f00
Personne17 <44241269+Personne17@users.noreply.github.com>
2019-06-25 18:37:00 +0200

    Add files via upload

---
+++ commands/rdel.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args) => {
+    
+    let msg = message;
+
+    if(msg.guild.member(msg.author) !== msg.guild.owner) {
+        msg.reply(" tu n'as pas les permissions requises pour effectuer cette commande. ('OWNER')")
+        return;
+    }
+    if(!msg.guild.me.hasPermission("MANAGE_ROLES")) {
+        msg.reply(" je n'ai pas la permission requise pour effectuer cette commande. ('MANAGE_CHANNELS')")
+        return;
+    }
+    msg.guild.roles.map(r => {
+        if(cr.deletable) {
+            cr.delete()
+        }
+    })
+
+}
+module.exports.help = {
+  name: "rdel"
+}
